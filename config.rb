4a96fab856655c93ea07d5e243baaf7a8124a548
bebun <wiramenggala.nugraha@gmail.com>
2016-04-07 10:02:52 +0700

    07 Apr 2016
    update untuk tag <button></button> di dalam form di ganti dengan tag
    <span></span>

---
+++ config.rb
@@ -0,0 +1,11 @@
+project_type = :stand_alone
+http_path = "/"
+sass_dir = "scss"
+css_dir = "css"
+images_dir = "images"
+fonts_dir = "font"
+javascript_dir = "js"
+line_comments = false
+preffered_syntax = :scss
+output_style = :expanded
+realtive_assets = true
